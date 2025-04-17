94b6e4513a8181e5108c09d452d9aed12c3fd405
Yaroslav Kaliuk <yaroslav.kalyuk@gmail.com>
2025-04-17 16:00:59 +0200

    added not-found

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,6 @@
-'use client';
-
-import { useEffect } from 'react';
-import { useRouter, usePathname } from 'next/navigation';
+import { redirect } from 'next/navigation';
 import { routing } from '@/i18n/routing';
 
 export default function Page() {
-  const router = useRouter();
-  const pathname = usePathname();
-
-  useEffect(() => {
-    const currentLocale = pathname.split('/')[1] || routing.defaultLocale;
-    router.push(`/${currentLocale}/dashboard`);
-  }, [router, pathname]);
+  redirect(`/${routing.defaultLocale}/dashboard`);
 }
