66db3fe2bb6682424eb4ece0ae27dc7d12fbcd11
Yaroslav Kaliuk <yaroslav.kalyuk@gmail.com>
2025-08-09 22:58:50 +0200

    fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export default async function RootLayout({
   const locale = await getLocale();
 
   return (
-    <html lang={locale}>
+    <html lang={locale} data-theme="dark">
       <body>{children}</body>
     </html>
   );
